33903b3fee5e8a3e2b2cb82383e6cb012a1193ae
Victor Hugo <victorhugobbpereira02@gmail.com>
2025-02-27 14:05:57 -0300

    desing e componentizção

--- view/index.php
+++ view/index.php
@@ -11,9 +11,9 @@
     <section class="vh-100 w-100 align-items-center justify-content-center">
         <?php include '../elements/navbar.php' ?>
         <div class="container text-center">
-            <div class="row row-cols-4 row-cols-lg-5 row-cols-sm-2 g-2 g-lg-2">
+            <div class="row row-cols-1 row-cols-lg-5 row-cols-sm-2 g-2 g-lg-2">
                 <div class="col">
-                    <div class="p-3 bg-primary rounded text-white">Row column</div>
+                    <div class="p-3 bg-primary rounded text-white fw-bolder">Row column</div>
                 </div>
                 <div class="col">
                     <div class="p-3">Row column</div>
