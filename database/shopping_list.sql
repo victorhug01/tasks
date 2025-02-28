bc0eff2771e7b87f49ed865cda491b4b9a7fb7b0
victor <victorhugobbpereira02@gmail.com>
2025-02-27 23:23:28 -0300

    adicionando scripts sql

---
+++ database/shopping_list.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS shoppinglist;
+
+USE shoppinglist;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    passwrd VARCHAR NOT NULL,
+    create_at DATETIME NOT NULL
+);
